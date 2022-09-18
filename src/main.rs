79b7162011bde73bdeb1c0a7d6c575414ec79f7d
prazdevs <sachabouillez@gmail.com>
2022-09-19 00:53:07 +0200

    feat: add write command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
 use color_eyre::eyre::Result;
+use structopt::StructOpt;
+
+/// A CLI for the growing and curation of a digital garden
+///
+/// Visit https://rustadventure.rs/garden for more!
+#[derive(StructOpt, Debug)]
+#[structopt(name = "garden")]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    /// Write something in your garden
+    ///
+    /// This command will open your $EDITOR, wait for you to write something, 
+    /// and then save the file to your garden
+    Write {
+        #[structopt(short, long)]
+        /// Optionally set a title for what you are going to write about
+        /// 
+        title: Option<String>,
+    },
+}
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+    let opt = Opt::from_args();
+    dbg!(opt);
     todo!();
 }
