5907689e2a45d75b510015739474ee951a9cbcf1
prazdevs <sachabouillez@gmail.com>
2022-09-23 14:43:14 +0200

    feat: add path option

--- README.md
+++ README.md
@@ -4,6 +4,18 @@ A CLI tool for the creation and maintenance of Digital Gardens.
 
 ## Commands
 
+### Setting the garden path
+
+```sh
+GARDEN_PATH=~/my-digital-garden garden write
+garden -p ~/my-digital-garden
+garden --garden_path ~/my-digital-garden write
+```
+
+### Write
+
+Open a new file to write in our digital garden.
+
 ```sh
 garden write
 garden write -t "Some Title"
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-use color_eyre::eyre::Result;
+use std::path::PathBuf;
+use color_eyre::eyre::{eyre, Result, Context};
 use digital_garden::write;
+use directories::UserDirs;
 use structopt::StructOpt;
 
 /// A CLI for the growing and curation of a digital garden
 ///
-/// Visit https://rustadventure.rs/garden for more!
+/// Visit https://github.com/prazdevs/digital_garden for more!
 #[derive(StructOpt, Debug)]
 #[structopt(name = "garden")]
 struct Opt {
+    #[structopt(
+        parse(from_os_str),
+        short = "p",
+        long,
+        env
+    )]
+    garden_path: Option<PathBuf>,
+
     #[structopt(subcommand)]
     cmd: Command,
 }
@@ -26,10 +36,23 @@ enum Command {
     },
 }
 
+fn get_default_garden_dir() -> Result<PathBuf> {
+    let user_dirs = UserDirs::new().ok_or_else(|| {
+        eyre!("Could not find home directory")
+    })?;
+    return Ok(user_dirs.home_dir().join(".garden"));
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
     let opt = Opt::from_args();
     dbg!(&opt);
+
+    let garden_path = match opt.garden_path {
+        Some(pathbuf) => Ok(pathbuf),
+        None => get_default_garden_dir().wrap_err("`garden_path` was not supplied")
+    }?;
+
     match opt.cmd {
         Command::Write { title } => write(title),
     }
